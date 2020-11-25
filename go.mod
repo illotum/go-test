23aa1905305b1ce009d2d5a1b3ab1147aa4534a9
Alex V <alex.valiushko@whiteops.com>
2020-11-25 14:41:15 -0800

    add diff test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/illotum/go-test
 
 go 1.14
+
+require github.com/google/go-cmp v0.5.4 // indirect
--- test.go
+++ test.go
@@ -1,8 +1,12 @@
+// Package test implements simple equality and diff routines for unit testing
+// in Go.
 package test
 
 import (
 	"reflect"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func Eq(t testing.TB, have, want interface{}) bool {
@@ -10,6 +14,15 @@ func Eq(t testing.TB, have, want interface{}) bool {
 	eq := reflect.DeepEqual(have, want)
 	if !eq {
 		t.Errorf("\nhave %+v\nwant %+v", have, want)
+	}
+	return eq
+}
+
+func Diff(t testing.TB, have, want interface{}) bool {
+	t.Helper()
+	eq := cmp.Equal(have, want)
+	if !eq {
+		t.Error(cmp.Diff(have, want))
 	}
 	return eq
 }
